23bcf2132403ea35868b13cf82811c4e82e673e2
Kareem Ebrahem <safen894@gmail.com>
2023-11-23 14:57:40 +0200

    a23: Update twrp_a23.mk

--- twrp_a23.mk
+++ twrp_a23.mk
@@ -6,17 +6,13 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a23 device
 $(call inherit-product, device/samsung/a23/device.mk)
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 PRODUCT_DEVICE := a23
 PRODUCT_NAME := twrp_a23
